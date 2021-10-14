9f31b9fac2feed9286e499dd64876dab123aea78
Julien Mahe Crenn <julien.mahecrenn@sky.uk>
2021-10-14 12:15:56 +0100

    adding Passenger class tests

--- airport.test.js
+++ airport.test.js
@@ -20,3 +20,18 @@ describe("Testing the Bag class and its child objects", () => {
         expect(typeof(suitcase)).toBe("object");
     });
 });
+
+describe("Testing for the Passenger class", () => {
+
+    test("Testing that bob is an instance of passenger", () => {
+        expect(bob).toBeInstanceOf(Passenger);
+    });
+
+    test("Testing the type of bob's bag attribut", () => {
+        expect(typeof(bob.bags)).toBe("array");
+    });
+
+    test("Testing that the array length is changed when a new bag is added to the passenger", () => {
+        expect(bob.bags.length).toBeGreaterThan(0);
+    });
+});
